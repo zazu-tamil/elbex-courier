d959b452cc0b0662f53cf1b407a8da5eb41ea8a4
subashvs7 <subash07070707@gmail.com>
2025-11-14 14:01:14 +0530

    database updated

--- application/config/database.php
+++ application/config/database.php
@@ -70,14 +70,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
+
 $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '192.168.0.37',
+	'username' => 'zazu',
+	'password' => 'zazu@123',
 	'database' => 'crit_elbex_tracking',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
